f07055008042faf0bdffaf783689067e708303f0
pinkjagnie <olga.krzysztofik@gmail.com>
2023-09-01 17:12:33 +0200

    Updated task.js

--- models/task.js
+++ models/task.js
@@ -1,24 +1,8 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Task extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Task.init({
+  const Task = sequelize.define("Task", {
     title: DataTypes.STRING,
-    description: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Task',
+    description: DataTypes.STRING,
   });
+
   return Task;
 };
